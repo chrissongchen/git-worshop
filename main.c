951732a333ad1e983edf9ce09e05bb223cb1a614
chrissongchen <schen13@e.ntu.edu.sg>
2017-03-31 15:00:53 +0800

    Implemented main function
    Implemented main function

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdib.h>
+
+void hello1(void)
+{
+	printf(stdout, "This is displayed on standard output\n);
+}
+
+int main(void)
+{
+	hellow();
+	return EXIT_SUCCESS;
+}
